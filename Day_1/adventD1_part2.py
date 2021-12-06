b54b82afac2bd92c496625d6ef7a489ae63afd09
Brittney jones <brittneyjones@Brittneys-MacBook-Pro.local>
2021-12-06 11:24:51 -0500

    updating d1 part 2 problem

--- Day_1/adventD1_part2.py
+++ Day_1/adventD1_part2.py
@@ -1,7 +1,7 @@
 count = 0
 
-with open('input.txt') as f:
-    lines = f.readlines()
+with open('input.txt') as input:
+    lines = input.readlines()
     for i in range(len(lines)):
         lines[i] = int(lines[i])
     for i in range(len(lines)-3):
@@ -9,6 +9,4 @@ with open('input.txt') as f:
         sliding_sum_2 = lines[i+1] + lines[i+2] + lines[i+3]
         if sliding_sum_1 < sliding_sum_2:
             count += 1
-    #print(count)
-    
-    print(len(lines))
+    print(count)
