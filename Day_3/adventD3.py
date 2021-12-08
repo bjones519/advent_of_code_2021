e7fbe1a8de2b7da9fcf966db64f135a9426d9831
Brittney jones <brittneyjones@Brittneys-MacBook-Pro.local>
2021-12-08 17:56:48 -0500

    Working on Day 3

--- Day_3/adventD3.py
+++ Day_3/adventD3.py
@@ -1,3 +1,16 @@
+power_consumption = 0
+gamma_rate = []
+epsilon_rate = []
+zero_count = 0
+one_count = 0
 
 with open('binary.txt') as input:
     lines = input.readlines()
+    for i in lines:
+        if i[0] == '0' and zero_count > one_count:
+            zero_count += 1
+            gamma_rate = ['0']
+        else: 
+            one_count += 1
+            gamma_rate = ['1']
+        print(gamma_rate)
