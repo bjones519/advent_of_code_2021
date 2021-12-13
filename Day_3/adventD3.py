7992ccc518f22be009481936275d2440151d92bd
Brittney jones <brittneyjones@Brittneys-MacBook-Pro.local>
2021-12-12 19:23:06 -0500

    Finishing Day3

--- Day_3/adventD3.py
+++ Day_3/adventD3.py
@@ -1,16 +1,30 @@
 power_consumption = 0
-gamma_rate = []
-epsilon_rate = []
-zero_count = 0
-one_count = 0
+gamma_rate = ''
+epsilon_rate = ''
+
 
 with open('binary.txt') as input:
     lines = input.readlines()
-    for i in lines:
-        if i[0] == '0' and zero_count > one_count:
-            zero_count += 1
-            gamma_rate = ['0']
-        else: 
-            one_count += 1
-            gamma_rate = ['1']
-        print(gamma_rate)
+    for index in range(12):
+        zero_count = 0
+        one_count = 0
+        for i in lines:
+            i = i.strip()
+            if i[index] == '0':
+                zero_count += 1
+                
+            else: 
+                one_count += 1
+                
+        if zero_count > one_count:
+            gamma_rate += '0'
+            
+        else:
+            gamma_rate += '1' 
+            
+        if zero_count < one_count:
+            epsilon_rate += '0'
+            
+        else:
+            epsilon_rate += '1'       
+        print((int(gamma_rate,2) * int(epsilon_rate,2)))
